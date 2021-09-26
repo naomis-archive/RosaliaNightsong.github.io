a2139ff17cf6e3ecc191d7283b2e8e18f3e184c0
Nicholas Carrigan <ncharrigan@gmail.com>
2021-09-26 20:43:34 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/RosaliaNightsong/RosaliaNightsong.github.io/commit/599596044aa4748df0efb958825c6b68acd45f37

--- assets/data/bio.ts
+++ assets/data/bio.ts
@@ -6,7 +6,23 @@ export interface BioInt {
 
 export const bioData: BioInt[] = [
   {
-    category: 'Coming Soon!',
-    data: 'TBA',
+    category: 'Introduction',
+    data: 'I am a young female paladin. My primary motivation is to protect and assist others, especially those who are weak or unfortunate. I travel across various lands seeking to bring some positivity to people. Many consider me to be very friendly, but my trusting nature tends to get me in trouble sometime. Thanks to the blessing of the goddess, I stopped ageing at 25.',
+  },
+  {
+    category: 'History',
+    data: "I grew up in a small town, with loving parents and close friends. Unfortunately, when I was 12 our village was raided by local bandits. Many of the people I was close to, including my parents, did not survive the assault. It was at this time that I swore to protect others from the same fate, and I set off to the local lord's palace to enlist in the royal guard. After three years of training, I was sent on my first mission to investigate another bandit attack. During that investigation, I came across a temple - it was there that I first connected with the Goddess and received my holy power.",
+  },
+  {
+    category: 'Appearance',
+    data: 'I have long black hair, dark green eyes, and pale skin. I always wear white, usually a robe during adventures and a dress when I have a more formal event. I do not wear makeup, as I prefer to spend my time helping others rather than making myself look good. Finally, I never wear shoes - I find that I can move much quicker without them.',
+  },
+  {
+    category: 'Skills and Combat',
+    data: 'As a paladin, I possess holy abilities that allow me to heal the wounded, turn away those of evil heart, and even bring someone back from the brink of death (on rare occasion). My chosen weapon is the greatsword, which allows me to strike fierce and true, smiting evil at the root. I have no need for armour, as I know the Goddess protects me, which allows me more agility than a typical fighter.',
+  },
+  {
+    category: 'Personality',
+    data: 'Many consider me to be Lawful Good, and a champion of justice. My primary goal is to help others, but I will not help them commit evil acts. Murder, theivery, and other harmful acts are unacceptable. Furthermore, if I discover that someone has attempted to deceive me into committing such wrongdoings, I bring fierce retribution their way. However, so long as a request does not malign with these values, I will do everything in my power to assist them. I often refuse payment, and only take enough to cover my basic needs when required.',
   },
 ];
