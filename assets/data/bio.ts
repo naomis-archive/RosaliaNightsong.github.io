4fc61804a2dcd95a493689fd4d23027d44cb1974
Nicholas Carrigan <ncharrigan@gmail.com>
2021-09-26 19:08:33 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/RosaliaNightsong/RosaliaNightsong.github.io/commit/b27cf4474f3a8112d69398c4dd2df5cd2f0af8c1

---
+++ assets/data/bio.ts
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+export interface BioInt {
+  category: string;
+  data: string;
+}
+
+export const bioData: BioInt[] = [
+  {
+    category: 'Coming Soon!',
+    data: 'TBA',
+  },
+];
