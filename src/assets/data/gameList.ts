f3c6efcde0502f21483f8910990c7044e5815546
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2022-01-09 20:08:35 -0800

    feat: add adventures component (#84)

---
+++ src/assets/data/gameList.ts
@@ -0,0 +1,14 @@
+export interface GameInt {
+  fileName: string;
+  gameName: string;
+  gameUrl: string;
+}
+
+export const GameList: GameInt[] = [
+  {
+    fileName: 'dos2.jpg',
+    gameName: 'Divinity: Original Sin II',
+    gameUrl:
+      'https://store.steampowered.com/app/435150/Divinity_Original_Sin_2__Definitive_Edition/',
+  },
+];
