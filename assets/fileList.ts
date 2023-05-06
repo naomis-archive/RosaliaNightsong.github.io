e0ecb713c9e8fe275de7d830d1afac41bcac75ce
Naomi Carrigan <ncharrigan@gmail.com>
2023-05-06 06:44:09 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/RosaliaNightsong/RosaliaNightsong.github.io/commit/ace617e7b1ce1b3db5f1a5e003fc24d429bb1d77

--- assets/fileList.ts
+++
@@ -1,6 +0,0 @@
-export const artFiles: string[] = [];
-export const emotesFiles: string[] = [];
-export const gamesFiles: string[] = [];
-export const outfitsFiles: string[] = [];
-export const posesFiles: string[] = [];
-export const tattooFiles: string[] = []
